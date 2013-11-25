7d413fc10e47014e91a8367741f4d1ebb198a1c4
Alex Ehrnschwender <alex.ehrnschwender@dena.com>
2013-11-25 10:10:05 -0800

    basic mocha test setup, fixed some function bugs

---
+++ test/acceptance.js
@@ -0,0 +1,89 @@
+"use strict";
+
+var assert = require('assert'),
+    _ = require('underscore'),
+    util = require('util'),
+    http = require('http');
+
+
+describe('Musicmetric', function(){
+
+    describe('#new()', function(){
+        it('can create a new Musicmetric instance with correct init options', function(cb){
+
+        });
+
+        it('cannot create a new Musicmetric instance without an API key', function(cb){
+
+        });
+
+    });
+
+    describe('#getTotalPlays()', function(){
+        it('can get artist total plays given a correct artist UUID', function(cb){
+
+        });
+        it('can get artist total plays given an artist name', function(cb){
+
+        });
+
+    });
+    describe('#getTotalFans()', function(){
+        it('can get artist total fans given a correct artist UUID', function(cb){
+
+        });
+        it('can get artist total fans given an artist name', function(cb){
+
+        });
+
+    });
+    describe('#getTotalDownloads()', function(){
+        it('can get artist total downloads given a correct artist UUID', function(cb){
+
+        });
+        it('can get artist total downloads given an artist name', function(cb){
+
+        });
+
+    });
+    describe('#getAgeDemographic()', function(){
+        it('can get artist age demographic given a correct artist UUID', function(cb){
+
+        });
+        it('can get artist age demographic given an artist name', function(cb){
+
+        });
+
+    });
+    describe('#getGenderDemographic()', function(){
+        it('can get artist gender demographic given a correct artist UUID', function(cb){
+
+        });
+        it('can get artist gender demographic given an artist name', function(cb){
+
+        });
+
+    });
+    describe('#getArtistTopCities()', function(){
+        it('can get artist top cities given a correct artist UUID', function(cb){
+
+        });
+        it('can get artist top cities given an artist name', function(cb){
+
+        });
+
+    });
+    describe('#getArtistCharts()', function(){
+        it('can get artist charts given a correct artist UUID', function(cb){
+            
+
+        });
+        it('can get artist charts given an artist name', function(cb){
+
+        });
+
+    });
+
+
+
+});
